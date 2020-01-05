31a501f2dd6aebbcf9f5e8b5e8fa8bdb965e80ec
Ritchie Young <ritchiey@users.noreply.github.com>
2020-01-05 17:39:37 +0800

    Refactor spec further

--- spec/objectively_spec.rb
+++ spec/objectively_spec.rb
@@ -139,65 +139,69 @@ RSpec.describe Objectively do # ~> NameError: uninitialized constant RSpec
       Objectively::Diagram.new
     end
 
-    let(:trace) do
-      stub_const 'A', Class.new
-      A.class_eval do
-        def initialize(output)
-          @output = output
-        end
+    context 'when we trace a short program' do
+      let(:trace) do
+        stub_const 'A', Class.new
+        A.class_eval do
+          def initialize(output)
+            @output = output
+          end
 
-        def do_it
-          @output.say 'hello'
-          @output.say 'world'
+          def do_it
+            @output.say 'hello'
+            @output.say 'world'
+          end
+        end
+        stub_const 'B', Class.new
+        B.class_eval do
+          def say(text)
+            puts(text)
+          end
+        end
+        a = A.new(B.new)
+        diagram.trace do
+          a.do_it
         end
       end
-      stub_const 'B', Class.new
-      B.class_eval do
-        def say(text)
-          puts(text)
+
+      describe 'and draw the trace' do
+        let(:draw) do
+          trace.draw(output: { png: 'trace.png' })
+        end
+
+        before do
+          allow(GraphViz).to receive(:new)
+            .and_yield(graph_viz)
+            .and_return(graph_viz)
+        end
+        let(:graph_viz) { instance_double('GraphViz') }
+
+        let(:node_a1) { double(:node_a1) }
+        let(:node_b1) { double(:node_b1) }
+        let(:node_example) { double(:node_example) }
+
+        it 'draws a directed graph of the calls' do
+          expect(graph_viz).to receive(:add_nodes)
+            .with('Example')
+            .and_return(node_example)
+          expect(graph_viz)
+            .to receive(:add_nodes)
+            .with('A:1')
+            .and_return(node_a1)
+          expect(graph_viz)
+            .to receive(:add_nodes)
+            .with('B:1')
+            .and_return(node_b1)
+          expect(graph_viz)
+            .to receive(:add_edges)
+            .with(node_example, node_a1, label: '1. do_it()')
+          expect(graph_viz)
+            .to receive(:add_edges)
+            .with(node_a1, node_b1, label: %[2. say("hello")\n3. say("world")])
+          expect(graph_viz).to receive(:output).with(png: 'trace.png')
+          draw
         end
       end
-      a = A.new(B.new)
-      diagram.trace do
-        a.do_it
-      end
-    end
-
-    let(:draw) do
-      trace.draw(output: { png: 'trace.png' })
-    end
-
-    before do
-      allow(GraphViz).to receive(:new)
-        .and_yield(graph_viz)
-        .and_return(graph_viz)
-    end
-    let(:graph_viz) { instance_double('GraphViz') }
-
-    let(:node_a1) { double(:node_a1) }
-    let(:node_b1) { double(:node_b1) }
-    let(:node_example) { double(:node_example) }
-
-    it 'draws a directed graph of the calls' do
-      expect(graph_viz).to receive(:add_nodes)
-        .with('Example')
-        .and_return(node_example)
-      expect(graph_viz)
-        .to receive(:add_nodes)
-        .with('A:1')
-        .and_return(node_a1)
-      expect(graph_viz)
-        .to receive(:add_nodes)
-        .with('B:1')
-        .and_return(node_b1)
-      expect(graph_viz)
-        .to receive(:add_edges)
-        .with(node_example, node_a1, label: '1. do_it()')
-      expect(graph_viz)
-        .to receive(:add_edges)
-        .with(node_a1, node_b1, label: %[2. say("hello")\n3. say("world")])
-      expect(graph_viz).to receive(:output).with(png: 'trace.png')
-      draw
     end
   end
 end
